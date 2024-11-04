0b3acc4a2db739dbd93d56ea488d88d4278ddf38
meta-aku <bakmy001@edu.xamk.fi>
2024-11-04 23:24:38 +0200

    04.11/23:23 -Player and Enemy show seperately in their places

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 function playRock() {
+    clear();
     enemy();
     document.getElementById("playerRock").innerHTML = "This works";
 }
 function playPaper() {
+    clear();
     enemy();
     document.getElementById("playerPaper").innerHTML = "This works";
 }
 function playScissors() {
+    clear();
     enemy();
     document.getElementById("playerScissors").innerHTML = "This works";
 
@@ -16,14 +19,26 @@ function enemy() {
     let enemyChoice = Math.floor(Math.random() * 3);
     console.log(enemyChoice);
     if (enemyChoice % 3 === 0) {
-        document.getElementById("enemyRock").innerHTML = enemyChoice + "This works";
+        document.getElementById("enemyRock").innerHTML = "This works";
     }
     if (enemyChoice % 3 === 1) {
-        document.getElementById("enemyPaper").innerHTML = enemyChoice + "This works";
+        document.getElementById("enemyPaper").innerHTML = "This works";
     }
     if (enemyChoice % 3 === 2) {
-        document.getElementById("enemyScissors").innerHTML = enemyChoice + "This works";
+        document.getElementById("enemyScissors").innerHTML = "This works";
     }
-        
+
     
+        
+}
+
+function clear() {
+
+    document.getElementById("playerRock").innerHTML = "<br>";
+    document.getElementById("playerPaper").innerHTML = "<br>";
+    document.getElementById("playerScissors").innerHTML = "<br>";
+
+    document.getElementById("enemyRock").innerHTML = "";
+    document.getElementById("enemyPaper").innerHTML = "";
+    document.getElementById("enemyScissors").innerHTML = "";
 }
