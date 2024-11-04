b8493e16bfb5654efdd1582fa1b66ff0172d0e1a
meta-aku <bakmy001@edu.xamk.fi>
2024-11-04 22:54:29 +0200

    04.11/22:54

---
+++ app.js
@@ -0,0 +1,9 @@
+function playRock() {
+    document.getElementById("playerRock").innerHTML = "This works";
+}
+function playPaper() {
+    document.getElementById("playerPaper").innerHTML = "This works";
+}
+function playScissors() {
+    document.getElementById("playerScissors").innerHTML = "This works";
+}
