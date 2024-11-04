409172e84ae2ef1dd427c88bdb17b8be8725c5a9
meta-aku <bakmy001@edu.xamk.fi>
2024-11-04 23:12:03 +0200

    04.11/23:11 -Random Enemy Works

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 function playRock() {
+    enemy();
     document.getElementById("playerRock").innerHTML = "This works";
 }
 function playPaper() {
+    enemy();
     document.getElementById("playerPaper").innerHTML = "This works";
 }
 function playScissors() {
+    enemy();
     document.getElementById("playerScissors").innerHTML = "This works";
+
+}
+
+function enemy() {
+    let enemyChoice = Math.floor(Math.random() * 3);
+    console.log(enemyChoice);
+    if (enemyChoice % 3 === 0) {
+        document.getElementById("enemyRock").innerHTML = enemyChoice + "This works";
+    }
+    if (enemyChoice % 3 === 1) {
+        document.getElementById("enemyPaper").innerHTML = enemyChoice + "This works";
+    }
+    if (enemyChoice % 3 === 2) {
+        document.getElementById("enemyScissors").innerHTML = enemyChoice + "This works";
+    }
+        
+    
 }
